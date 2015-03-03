4a0b4c558b1e05cdaba4ad2baae23a17067cf3be
mrjavoman <mrjavoman@hotmail.com>
2015-03-02 18:59:09 -0600

    CompletedCSharpClassifier

--- Test/Program.cs
+++ Test/Program.cs
@@ -10,9 +10,21 @@ namespace DigitRecognizer
     {
         static void Main(string[] args)
         {
+            var distance = new ManhattanDistance();
+            var classifier = new BasicClassifier(distance);
+
             var trainingPath = @"C:\Users\xavi\Dropbox\Dev work\Machine Learning for .NET Developers\trainingsample.csv";
             var training = DataReader.ReadObservations(trainingPath);
-            
+
+            classifier.Train(training);
+
+            var validationPath = @"C:\Users\xavi\Dropbox\Dev work\Machine Learning for .NET Developers\validationsample.csv";
+            var validation = DataReader.ReadObservations(validationPath);
+
+            var correct = Evaluator.Correct(validation,classifier);
+
+            Console.WriteLine("Correctly classified: {0:P2}", correct);
+
             Console.ReadLine();
         }
     }
