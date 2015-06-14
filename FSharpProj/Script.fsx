92db40dd5c415bfa802a0bfa17d81299894437cb
Xavi <mrjavoman@hotmail.com>
2015-06-14 16:37:11 -0500

    completed F# code and added validation

--- FSharpProj/Script.fsx
+++ FSharpProj/Script.fsx
@@ -19,5 +19,28 @@ let reader path =
     data.[1..]
     |> Array.map toObservation
 
-let trainingPath = @"C:\Users\xavi\Dropbox\Dev work\Machine Learning for .NET Developers\trainingsample.csv";
-let trainingData = reader trainingPath
+let manhattanDistance (pixels1, pixels2) =
+    Array.zip pixels1 pixels2
+    |> Array.map (fun(x,y) -> abs(x-y))
+    |> Array.sum
+
+let train (trainingset:Observation[])=
+    let classify (pixels:int[]) =
+        trainingset
+        |> Array.minBy(fun x -> manhattanDistance (x.Pixels, pixels))
+        |> fun x -> x.Label
+    classify
+
+
+
+let trainingPath = @"C:\Users\xavi\Dropbox\Dev work\Machine Learning for .NET Developers\trainingsample.csv"
+let trainingData = reader trainingPath;;
+
+let classifier = train trainingData;;
+
+let validationPath = @"C:\Users\xavi\Dropbox\Dev work\Machine Learning for .NET Developers\validationsample.csv"
+let validationData = reader validationPath;;
+
+validationData
+|> Array.averageBy (fun x -> if classifier x.Pixels = x.Label then 1. else 0.)
+|> printfn "Correct: %.3f"
