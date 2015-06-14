2bfe272652656939b6b1cce9a214a262adec7b1d
Xavi <mrjavoman@hotmail.com>
2015-06-14 15:37:37 -0500

    Added F# code for reading data into an Observation

--- FSharpProj/Script.fsx
+++ FSharpProj/Script.fsx
@@ -1,10 +1,23 @@
 ﻿// Learn more about F# at http://fsharp.net. See the 'F# Tutorial' project
 // for more guidance on F# programming.
 
-#load "Library1.fs"
-open FSharpProj
+//#load "Library1.fs"
+//open FSharpProj
 
-// Define your library scripting code here
-let x = 42
+open System.IO
 
-let y = x + 100
+type Observation = {Label:string; Pixels:int[]}
+
+let toObservation(csvData:string) = 
+    let columns = csvData.Split(',')
+    let label = columns.[0]
+    let pixels = columns.[1..] |> Array.map int
+    {Label = label; Pixels= pixels}
+
+let reader path = 
+    let data = File.ReadAllLines path
+    data.[1..]
+    |> Array.map toObservation
+
+let trainingPath = @"C:\Users\xavi\Dropbox\Dev work\Machine Learning for .NET Developers\trainingsample.csv";
+let trainingData = reader trainingPath
