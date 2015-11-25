9a62a87d271da5bf1dfd9e9a2799e3083750865d
Xavi <mrjavoman@hotmail.com>
2015-11-24 20:18:14 -0600

    added an extra distance function and a solution file

--- FSharpProj/Script.fsx
+++ FSharpProj/Script.fsx
@@ -19,15 +19,23 @@ let reader path =
     data.[1..]
     |> Array.map toObservation
 
+type Distance = int[] * int[] -> int
+
 let manhattanDistance (pixels1, pixels2) =
     Array.zip pixels1 pixels2
     |> Array.map (fun(x,y) -> abs(x-y))
     |> Array.sum
 
-let train (trainingset:Observation[])=
+let eucledianDistance(pixels1,pixels2) =
+    Array.zip pixels1 pixels2
+    |> Array.map (fun (x,y) -> pown(x-y) 2)
+    |> Array.sum
+    //|> sqrt
+
+let train (trainingset:Observation[]) (dist:Distance)=
     let classify (pixels:int[]) =
         trainingset
-        |> Array.minBy(fun x -> manhattanDistance (x.Pixels, pixels))
+        |> Array.minBy(fun x -> dist (x.Pixels, pixels))
         |> fun x -> x.Label
     classify
 
@@ -36,11 +44,24 @@ let train (trainingset:Observation[])=
 let trainingPath = @"C:\Users\xavi\Dropbox\Dev work\Machine Learning for .NET Developers\trainingsample.csv"
 let trainingData = reader trainingPath;;
 
-let classifier = train trainingData;;
+//let classifier = train trainingData;;
+let manhattanClassifier = train trainingData manhattanDistance;;
+let eucledianClassifier = train trainingData eucledianDistance;;
 
 let validationPath = @"C:\Users\xavi\Dropbox\Dev work\Machine Learning for .NET Developers\validationsample.csv"
 let validationData = reader validationPath;;
 
-validationData
-|> Array.averageBy (fun x -> if classifier x.Pixels = x.Label then 1. else 0.)
+printfn "Manhattan"
+validationData 
+|> Array.averageBy (fun x -> if manhattanClassifier x.Pixels = x.Label then 1. else 0.)
 |> printfn "Correct: %.3f"
+
+printfn "Euclidean"
+validationData 
+|> Array.averageBy (fun x -> if eucledianClassifier x.Pixels = x.Label then 1. else 0.)
+|> printfn "Correct: %.3f"
+
+
+//validationData
+//|> Array.averageBy (fun x -> if classifier x.Pixels = x.Label then 1. else 0.)
+//|> printfn "Correct: %.3f"
